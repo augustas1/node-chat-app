bfe827ff9755ec1de829b688524808d7fccfabe2
Augustas Skaburskas <augustas.skaburskas@gmail.com>
2018-02-25 15:36:30 +0200

    Send messages to onyl people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -75,7 +75,6 @@ jQuery('#message-form').on('submit', (e) => {
     const messageTextBox = jQuery('[name=message]');
 
     socket.emit('createMessage', {
-        from: 'User',
         text: messageTextBox.val()
     }, () => {
         messageTextBox.val('');
